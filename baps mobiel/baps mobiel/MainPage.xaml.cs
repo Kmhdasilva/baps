9663d39a849d74d6b32156a56bc90c46ca21d2b4
Kevin <kmh.nunesdasilva@gmail.com>
2020-11-27 13:01:15 +0100

    project startup

---
+++ baps mobiel/baps mobiel/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace baps_mobiel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            
+        }
+    }
+}
